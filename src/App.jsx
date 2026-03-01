9f987d863b27712a264fb1ee5a2232b52dd346bf
parasdeep1075-netizen <parasdeep1075@gmail.com>
2026-03-02 03:55:17 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{
+      minHeight: "100vh",
+      background: "linear-gradient(135deg, #0f2027, #203a43, #2c5364)",
+      color: "white",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      fontFamily: "Arial"
+    }}>
+      <h1>PUGKC Smart Mess ERP 🚀</h1>
+    </div>
+  );
+}
+
+export default App;
